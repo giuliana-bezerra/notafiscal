2a6ac3e406acc32df36420428d8c7e0032f3ca44
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2019-01-30 10:20:43 -0300

    Aula 05

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,7 @@
 import { log } from './utils/logger.js';
 import { notasService as service} from './service/nota.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { timeoutPromise, retry } from './handler/promise-handler.js';
 
 const operations = pipe (
     partialize(takeUntil, 3),
@@ -8,8 +9,7 @@ const operations = pipe (
 );
 
 const action = operations(() =>
-    service
-    .sumItems(2143)
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems(2143)))
     .then(log)
     .catch(log)
 );
--- public/app/handler/promise-handler.js
+++ public/app/handler/promise-handler.js
@@ -1,2 +1,23 @@
 export const handleStatus = res =>
     res.ok ? res.json() : Promise.reject(res.textContent);
+
+export const timeoutPromise = (miliseconds, promise) => {
+    const timeout = new Promise((resolve, reject) =>
+        setTimeout(() => reject(`Timeout! Limite de tempo ultrapassou ${miliseconds}`), miliseconds)
+    );
+    return Promise.race([promise, timeout]);
+};
+
+export const delay = miliseconds => data =>
+    new Promise((resolve, reject) => 
+        setTimeout(() => resolve(data), miliseconds));
+
+export const retry = (retries, miliseconds, fn) =>
+    fn().catch(err => {
+        console.log(retries);
+        return delay(miliseconds)().then(
+            () => retries > 1 
+            ? retry(--retries, miliseconds, fn) 
+            : Promise.reject(err)
+        );
+    });
