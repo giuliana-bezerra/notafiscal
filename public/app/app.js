4e450c2562413364618836d9da99fed2f023b47a
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2019-01-30 16:38:58 -0300

    Aula 07

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,7 @@ import { notasService as service} from './service/nota.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 import { timeoutPromise, retry } from './handler/promise-handler.js';
 import { EventEmitter } from './utils/event-emitter.js';
+import { Maybe } from './utils/maybe.js';
 
 const operations = pipe (
     partialize(takeUntil, 3),
--- public/app/service/nota.js
+++ public/app/service/nota.js
@@ -1,17 +1,27 @@
 import { handleStatus } from '../handler/promise-handler.js';
 import { partialize } from "../utils/operators.js";
 import { compose, pipe } from "../utils/operators.js";
+import { Maybe } from '../utils/maybe.js';
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM => 
+    notasM.map(notas => 
+        notas.flatMap(nota => nota.itens)
+    );
+
+const filterItemsByCode = (code, itemsM) =>
+    itemsM.map(items => items.filter(item => item.codigo == code));
+
+const sumItemsValue = itemsM => 
+    itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notasService = {
     listAll() {
         return fetch(API)
         .then(handleStatus)
+        // retorna um `Maybe`
+        .then(notas => Maybe.of(notas))
         .catch(err => {
             console.log(err);
             return Promise.reject('Não foi possível obter as notas fiscais.');
@@ -27,6 +37,8 @@ export const notasService = {
             filterItems,
             sumItemsValue 
         );
-        return this.listAll().then(sumItems);
+        return this.listAll()
+            .then(sumItems)
+            .then(result => result.getOrElse(0));
     }
 };
