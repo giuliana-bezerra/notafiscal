05fac30c61d4079698288b4073a0df05cdd4f399
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2019-01-30 15:32:16 -0300

    Aula 06

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,7 @@ import { log } from './utils/logger.js';
 import { notasService as service} from './service/nota.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 import { timeoutPromise, retry } from './handler/promise-handler.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe (
     partialize(takeUntil, 3),
@@ -10,7 +11,8 @@ const operations = pipe (
 
 const action = operations(() =>
     retry(3, 3000, () => timeoutPromise(200, service.sumItems(2143)))
-    .then(log)
+    // Padrão Publisher/Subscriber - Publisher
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(log)
 );
 
