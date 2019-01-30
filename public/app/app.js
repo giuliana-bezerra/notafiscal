1af6a478a05dadf83aa3f682aa104a3477d71b4a
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2019-01-30 08:07:10 -0300

    Aula 04

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,17 @@
-import { handleStatus } from './handler/promise-handler.js';
 import { log } from './utils/logger.js';
 import { notasService as service} from './service/nota.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-document.querySelector('#myButton').onclick = () =>
+const operations = pipe (
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
     service
     .sumItems(2143)
     .then(log)
-    .catch(log);
+    .catch(log)
+);
+
+document.querySelector('#myButton').onclick = action;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -6,3 +6,15 @@ export const compose = (...fns) => value =>
 
 export const pipe = (...fns) => value => 
     fns.reduce((previuosValue, fn) => fn(previuosValue), value);
+
+export const takeUntil = (times, fn) =>
+    () => times-- > 0 && fn();
+
+// Para agendar a execução de uma função
+export const debounceTime = (miliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, miliseconds);
+    }
+}
